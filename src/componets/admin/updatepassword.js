4616df94222c2f8060b735cc73eb07829bc4a642
dev9ktms <124072547+dev9ktms@users.noreply.github.com>
2023-02-08 22:43:00 +0530

    Update updatepassword.js

--- src/componets/admin/updatepassword.js
+++ src/componets/admin/updatepassword.js
@@ -11,7 +11,7 @@ const UpdatePassword = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch("http://localhost:5000/admin/passwordreset", {
+        const response = await fetch("https://ed-tech-service-backend.onrender.com/passwordreset", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
